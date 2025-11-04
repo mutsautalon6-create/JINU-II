3a707f3226433a1693d211fadb9ddbe2c3f5c7ff
TALON <mutsautalon6@gmail.com>
2025-11-04 08:29:02 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,14 +1,14 @@
 const settings = {
-  packname: 'JINU-II',
-  author:'ɢʟᴀᴅᴅʏ ᴋɪɴɢ',
-  botName: "ᴊɪɴᴜ-ɪɪ",
-  botOwner: 'ɢʟᴀᴅᴅʏ ᴋɪɴɢ', // Your name
-  ownerNumber: '263775953409', //Set your number here without + symbol, just add country code & number without any space
+  packname: 'JINU-11',
+  author:'Skarta',
+  botName: "SKARTA23",
+  botOwner: 'Mutsau Talon', // Your name
+  ownerNumber: '263787780586', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "1.0.0",
- updateZipUrl: "https://github.com/GladdyKing/JINU-II/archive/refs/heads/main.zip",
+ updateZipUrl: "https://github.com/mutsautalon6-create/JINU-II/archive/refs/heads/main.zip",
 };
 
 module.exports = settings;
